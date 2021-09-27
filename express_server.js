769a624507601de342557a32152fe29c1aee512b
ShannaJSmith <shanna_smith987@hotmail.com>
2021-09-27 20:34:51 +0000

    added second route. urls/:shortURL

--- express_server.js
+++ express_server.js
@@ -14,8 +14,13 @@ app.get("/", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase.b2xVn2 };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
 });
 
 app.get("/urls.json", (req, res) => {
